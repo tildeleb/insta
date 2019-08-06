9ef13858744f02064c9e6e97c29fa4a78a1f8584
Lawrence E. Bakst <leb@me.com>
2019-08-06 17:09:30 -0400

    v1.0 is a trivial version

--- insta.go
+++ insta.go
@@ -1,8 +1,6 @@
 // Copyright © 2019 Lawrence E. Bakst. All rights reserved.
 
-// This code is a simulation model of Instagram and probably Twitter too
-// I didn't discuss or Goggle anything about how Instagram or Twitter are implemented
-// The only thing I Googled where some stats about Insta and other high volume users besides Taylor Swift
+// Simulate a service like Instagram or Twitter
 
 // Insta facts
 // about 1 billion users or 1 Gusers
@@ -38,19 +36,21 @@
 // non chronological order timeline
 
 // Assumptions and Approach
+// posting doesn't require much resource if no followers view it, it's inherently lazy, which is a huge plus
 // The main operations are:
 // 1. a user viewing their timeline, we assume chronological order
 // 2. a user making a post(s)
+// 3. follow or unfollow
+// 4. delete a post
 // 3 kinds of instances:
-// 1. Workers, to handle views and posts
+// 1. Workers, to handle requests from users to either view their timeline or make a post
 // 2. DB, stores data about users and posts
 // 3. Image, maps hash of picture to url and has cache of popular and recent images
-// Users are bimodal so partition users into two groups based on a load factor threshold
+// users are bimodal so perhaps partition users into two groups based on a load factor threshold
 // user load factor is followers * posts per day * views per day
 // cache goal is to reduce load on DB, image, and worker servers
-// 2 level caching, in memory on workers and SSD on worker, image and DB servers, eg avoid hitting DB and S3 for images
+// 2 level caching, in memory and SSD on worker, image and DB servers, eg avoid hitting DB and S3
 // fields we need from DB are current timestamp of large users and their most recent posts and their associated timestamps
-// posting doesn't mean much if no users try to view, it's inherently lazy, which is a huge plus
 
 // Solution
 // I can't simulate 1GUsers on my laptop so I will reduce everything by 1000 to 1M users
@@ -63,12 +63,14 @@
 // in chronological order.
 
 // Todo
+// deleted posts
+// folowing and unfollowing
 // no multiple worker instances
 // no image servers
 // DB not modeled as a server yet
 // no graph of users
 // Still doesn't accurately simulate how a scale Insta could really work
-// Group users into segments and distribute segments to multiple workers
+// shard users into segments and distribute segments to multiple workers
 // Caching of images and tweets at several levels
 
 // Given n users and m workers assign n/m users to each server to simulate a load balancer
@@ -95,7 +97,7 @@ var h = jenkins.New364(0)
 var db DB
 
 // rbetween returns random int [a, b]
-func rbetween(a int, b int) int {
+func rbetween(r *rand.Rand, a int, b int) int {
 	return r.Intn(b-a+1) + a
 }
 
@@ -119,15 +121,15 @@ const (
 	VIEW = "view" // view new posts
 )
 
-// represents the database entry for a user, think of the slices as sorted columns
+// represents the database entry for a user, think of the slices as columns sorted on timestamp
 // no keys yet, just linear search
 type user struct {
 	name       string
 	uid        int
 	timestamp  int64 // timestamp of latest post
+	followers  []int
 	following  []int
 	timestamps []int64 // parallel array to following, timestamp of last post of user we are following
-	followers  []int
 	posts      []*post
 }
 
@@ -163,6 +165,7 @@ type post struct {
 }
 
 type DB struct {
+	sync.Mutex
 	uid       int
 	lastTime  int64
 	postID    int
@@ -170,27 +173,20 @@ type DB struct {
 	followers []*user // used by sim for now
 }
 
+// assumes lock is held
 func (db *DB) GetUser(uid int) *user {
 	//fmt.Printf("GetUser: uid=%d, len(users)=%d\n", uid, len(users))
 	return db.users[uid]
 }
 
-func (db *DB) GetPostsAfter(uid int, timestamp int64) (posts []*post) {
-	u := db.GetUser(uid)
-	posts = u.postsAfter(timestamp)
-	return
-}
-
+// assumes lock is held
 func (db *DB) UpdateTimestamp(uid int, timestamp int64) {
 	u := db.GetUser(uid)
 	u.timestamp = timestamp
 }
 
-func (db *DB) NumberOfUsers() int {
-	return len(db.users)
-}
-
 // NewUser allocates and stores a new user in the DB
+// assumes lock is held
 func (db *DB) NewUser(name string) *user {
 	u := &user{name: name, uid: db.uid}
 	db.uid++
@@ -198,16 +194,35 @@ func (db *DB) NewUser(name string) *user {
 	return u
 }
 
+func (db *DB) GetPostsAfter(uid int, timestamp int64) (posts []*post) {
+	db.Lock()
+	defer db.Unlock()
+	u := db.GetUser(uid)
+	posts = u.postsAfter(timestamp)
+	return
+}
+
+func (db *DB) NumberOfUsers() int {
+	db.Lock()
+	defer db.Unlock()
+	return len(db.users)
+}
+
 // MakeUsers makes users who are just followers (for now)
-func (db *DB) MakeUsers(n int) {
+func (db *DB) MakeUsers(n int) []*user {
+	db.Lock()
+	defer db.Unlock()
 	for i := 0; i < n; i++ {
 		name := fmt.Sprintf("follower%d", i)
 		db.followers = append(db.followers, db.NewUser(name))
 	}
+	return db.followers
 }
 
 // MakeUser makes famous users who have lots of followers
 func (db *DB) MakeUser(name string, nfollowers int) *user {
+	db.Lock()
+	defer db.Unlock()
 	user := db.NewUser(name)
 	for i := 0; i < nfollowers; i++ {
 		u := db.followers[i]
@@ -217,6 +232,8 @@ func (db *DB) MakeUser(name string, nfollowers int) *user {
 }
 
 func (db *DB) Post(uid int, tweet string) {
+	db.Lock()
+	defer db.Unlock()
 	u := db.GetUser(uid)
 	t := time.Now().UnixNano()
 	if t == db.lastTime {
@@ -225,53 +242,74 @@ func (db *DB) Post(uid int, tweet string) {
 		db.lastTime = t
 	}
 	p := &post{pid: db.postID, uid: u.uid, text: tweet, fingerprint: hash(tweet), timestamp: t, deleted: false}
-	//fmt.Printf("post: pid=%d, ts=%d, text=%q\n", p.pid, p.timestamp, p.text)
-	u.timestamp = t
+	tim := time.Unix(0, p.timestamp)
+	fmt.Printf("Post: ts=%v, pid=P%d, name=%q, text=%q\n", tim, p.pid, u.name, p.text)
+	db.UpdateTimestamp(u.uid, t)
+	//u.timestamp = t // call update timestamp
 	db.postID++
 	u.posts = append(u.posts, p)
 }
 
 // View returns all the new posts on a users timeline in chronological order
 func (db *DB) View(uid int) (timeline []*post) {
-	u := db.GetUser(uid)
 	var aposts [][]*post
 	var ids []int
 	var users = make(map[int]*user)
+	var t1, t2 time.Time
+
+	db.Lock()
+	defer db.Unlock()
+	u := db.GetUser(uid)
 
 	// find all users that have new posts and get the posts we haven't seen
 	for i, id := range u.following {
 		f := db.GetUser(id)
 		users[id] = f
-		//fmt.Printf("view: user %q followng %q\n", u.name, f.name)
+		fmt.Printf("View: user %q followng %q\n", u.name, f.name)
 		ts := u.timestamps[i]
 		if f.timestamp == ts {
-			fmt.Printf("no new posts from %q\n", f.name)
+			fmt.Printf("View: no new posts from %q\n", f.name)
 			continue
 		}
 		posts := f.postsAfter(ts)
+		fmt.Printf("View: %d new posts from %q\n", len(posts), f.name)
 		u.timestamps[i] = posts[len(posts)-1].timestamp // update DB
 		aposts = append(aposts, posts)
 		ids = append(ids, id)
 	}
 
-	// the posts are sorted so merge them into a chronological timeline
-	for {
-		cnt := 0
-		for i, posts := range aposts {
-			if len(posts) != 0 {
-				p := aposts[i][0]
-				timeline = append(timeline, p)
-				fmt.Printf("user %q saw that %q posted post P%d contains %q, ts=%d\n",
-					u.name, users[p.uid].name, p.pid, p.text, p.timestamp)
-				aposts[i] = aposts[i][1:]
-				cnt++
+	// the posts and timestamps are sorted (by timestamp) so merge them into a chronological timeline
+	if len(aposts) > 0 {
+		for {
+			cnt := 0
+			idx := -1
+			for i, posts := range aposts {
+				if len(posts) != 0 {
+					if cnt == 0 {
+						t1 = time.Unix(0, aposts[i][0].timestamp)
+						idx = i
+					} else {
+						t2 = time.Unix(0, aposts[i][0].timestamp)
+						if t1.After(t2) {
+							idx = i
+							t1 = t2
+						}
+					}
+					cnt++
+				}
 			}
+			if cnt == 0 {
+				break
+			}
+			p := aposts[idx][0]
+			timeline = append(timeline, p)
+			tim := time.Unix(0, p.timestamp)
+			fmt.Printf("View: ts=%v, user %q saw that %q posted post P%d contains %q\n",
+				tim, u.name, users[p.uid].name, p.pid, p.text)
+			aposts[idx] = aposts[idx][1:]
 		}
-		if cnt == 0 {
-			break
-		}
+		fmt.Printf("\n")
 	}
-	fmt.Printf("\n")
 	return timeline
 }
 
@@ -317,21 +355,39 @@ func (ws *workerServer) start(msgs chan msg) {
 // code below here is very simple Instagram simulator
 //
 
-func tweeter(ch chan msg, es, ts *user) {
+func tweeter(ch chan msg, stars []*star) {
+	var s = rand.NewSource(time.Now().UTC().UnixNano())
+	var r = rand.New(s)
 	for {
-		ch <- msg{op: POST, id: ts.uid, tweet: "foo"}
-		ch <- msg{op: POST, id: es.uid, tweet: "bar"}
-		ch <- msg{op: POST, id: ts.uid, tweet: "baz"}
-		ch <- msg{op: POST, id: es.uid, tweet: "quux"}
-		time.Sleep(2 * time.Second)
+		star := stars[rbetween(r, 0, len(stars)-1)]
+		uid := star.user.uid
+		switch rbetween(r, 1, 5) {
+		case 5:
+			ch <- msg{op: POST, id: uid, tweet: "foo"}
+			fallthrough
+		case 4:
+			ch <- msg{op: POST, id: uid, tweet: "bar"}
+			fallthrough
+		case 3:
+			ch <- msg{op: POST, id: uid, tweet: "baz"}
+			fallthrough
+		case 2:
+			ch <- msg{op: POST, id: uid, tweet: "quux"}
+			fallthrough
+		case 1:
+			ch <- msg{op: POST, id: uid, tweet: "foobar"}
+		}
+		time.Sleep(time.Duration(rbetween(r, 1, 2)) * time.Second)
 	}
 }
 
 func viewer(ch chan msg) {
+	var s = rand.NewSource(time.Now().UTC().UnixNano())
+	var r = rand.New(s)
 	idx := 0
 	for {
-		r := rbetween(0, db.NumberOfUsers()-1)
-		u := db.GetUser(r)
+		auser := rbetween(r, 0, db.NumberOfUsers()-1)
+		u := db.GetUser(auser)
 		ch <- msg{op: VIEW, id: u.uid, tweet: ""}
 		u = db.GetUser(idx)
 		ch <- msg{op: VIEW, id: u.uid, tweet: ""}
@@ -339,13 +395,19 @@ func viewer(ch chan msg) {
 		if idx > db.NumberOfUsers()-1 {
 			idx = 0
 		}
-		time.Sleep(1 * time.Second)
+		time.Sleep(time.Duration(rbetween(r, 1, 2)) * time.Second)
 	}
 }
 
 const maxUsers = 10
-const tsFollowers = 5
-const esFollowers = 3
+
+type star struct {
+	name      string
+	followers int
+	user      *user
+}
+
+var stars = []*star{&star{"Taylor Swift", 5, nil}, &star{"Ed Sheeran", 3, nil}, &star{"Beyoncé", 7, nil}}
 
 // simulate Instagram
 func sim() {
@@ -354,11 +416,12 @@ func sim() {
 	db.MakeUsers(maxUsers) // make followers for stars
 
 	// make a few stars
-	ts := db.MakeUser("Taylor Swift", tsFollowers)
-	es := db.MakeUser("Ed Sheeran", esFollowers)
-	wg.Add(2)
+	for _, star := range stars {
+		star.user = db.MakeUser(star.name, star.followers)
+	}
 
-	go tweeter(ch, es, ts)
+	wg.Add(2)
+	go tweeter(ch, stars)
 	go viewer(ch)
 	wg.Wait() // currently never terminates, use ^C
 }
